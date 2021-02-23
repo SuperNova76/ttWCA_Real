71442404feabaf467c4ae154828d0658262cc904
Fabio Cardillo <fabio.cardillo@cern.ch>
2021-02-23 13:54:39 +0100

    -

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 export REL="21.2.160"
 export SOURCE_DIR="uctanalysistop"
 
-if [ ! $ATLAS_LOCAL_ROOT_BASE ]; then 
+if [ ! $ATLAS_LOCAL_ROOT ]; then 
     echo "--> get voms-proxy"
     voms-proxy-init -voms atlas
     
@@ -14,13 +14,13 @@ if [ ! $ATLAS_LOCAL_ROOT_BASE ]; then
 fi
 
 if [ ! $AnalysisBase_SET_UP ]; then
-    echo "--> AnalysisBase Setup"
+    echo "--> AnalysisBase setup"
     cd ${SOURCE_DIR}
     asetup "AnalysisBase,${REL},here"
     cd ..
 fi
 
-echo "--> cmake Compile"
+echo "--> cmake compile"
 if [ ! -d build ]; then mkdir build; fi
 cd build
 cmake ../${SOURCE_DIR}
